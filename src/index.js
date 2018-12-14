a63bdce1f7a4ff5f896cec9a90f815823434f665
Hapachino <erickwbrownfield@gmail.com>
2018-12-14 10:51:46 -0800

    react-intro | Erick Brownfield

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const erick = {
+  name: 'Erick',
+  luckyNumber: luckyNumber(),
+}
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
-    document.getElementById('root')
+  <Greeting user={erick} />,
+  document.getElementById('root')
 );
+
+function luckyNumber() {
+  return (Math.random() * (1000 - 1 + 1) | 0) + 1;
+}
+
+function Greeting(props) {
+  return (
+    <div className="container">
+      <h1>Welcome {props.user.name}</h1>
+      <h2 className="text-muted">Your lucky number is: {props.user.luckyNumber}</h2>
+    </div>
+  );
+}
+
