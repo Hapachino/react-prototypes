9acb922cf9563d840cb1246bdf9a342df9e49fec
Hapachino <erickwbrownfield@gmail.com>
2018-12-14 11:57:49 -0800

    functional-components | Erick Brownfield

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
+import Table from './table';
 
+const students = [
+  {
+    student: 'Erick',
+    course: 'C++',
+    grade: 100,
+  },
+  {
+    student: 'Skywalker',
+    course: 'The Force',
+    grade: 100,
+  },
+  {
+    student: 'Kobe',
+    course: 'Basketball',
+    grade: 100,
+  },
+];
 
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Grade Table</h1>
+      <Table data={students}/>
+    </div>
+  );
+}
+
+export default App;
