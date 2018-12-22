55dc2441428edd74c4dc3c75296307a8d60f7a88
Hapachino <erickwbrownfield@gmail.com>
2018-12-21 19:29:08 -0800

    routing-intro | Erick Brownfield

---
+++ src/components/gifts_parties.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => {
+  return (
+    <div>
+      <h1>Gifts & Parties</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero architecto reiciendis error officiis tempora amet pariatur, vel praesentium odit a quod distinctio similique cupiditate molestias consectetur illo incidunt perspiciatis deserunt.</p>
+    </div>
+  );
+}
